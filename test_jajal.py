e34d235b63dd5fbf8f6905b288d7cddb9a67bf08
insanazis19 <insan@gradienttech.io>
2023-07-31 23:39:53 +0700

    oke ga nih

--- test_jajal.py
+++ test_jajal.py
@@ -1,4 +1,7 @@
 from main import jajal
+from dotenv import load_dotenv
+import os
+load_dotenv(dotenv_path=".env.test")
 
 def test_jajal_success():
     result = jajal()
@@ -6,4 +9,5 @@ def test_jajal_success():
 
 def test_jajal_failed():
     result = jajal()
+    print(os.getenv("WADIDAW"), "<<<<<<<<< env")
     assert result != None
