b2560f1487197c4ea510bf1b80fa3f9ca136e52c
insanazis19 <insan@gradienttech.io>
2023-08-01 13:44:20 +0700

    add env and test

--- test_jajal.py
+++ test_jajal.py
@@ -1,6 +1,8 @@
 from main import jajal
 from dotenv import load_dotenv
 import os
+import json
+
 load_dotenv(dotenv_path=".env.test")
 
 def test_jajal_success():
@@ -12,3 +14,11 @@ def test_jajal_failed():
     print(os.getenv("WADIDAW"), "<<<<<<<<< env")
     print(os.getenv("WATITSUYA"), "<<<<<<<<< eWWW")
     assert result != None
+
+def test_read_json():
+    j_file = os.getenv("FIREBASE_ADMIN_SDK_FILENAME_TEST")
+    print(os.getenv("FIREBASE_ADMIN_SDK_FILENAME_TEST"), "<<<<<<<<< FIREBASE_ADMIN_SDK_FILENAME_TEST")
+    with open(j_file) as f:
+        data = json.load(f)
+
+    print(data), "<<<<<<<<< data FIREBASE_ADMIN_SDK_FILENAME_TEST")
